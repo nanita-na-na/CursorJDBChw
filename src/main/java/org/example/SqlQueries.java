d54c88ca4d0d3a2a54b481df03447b0c0e49199f
Yanita <yanita2823@gmail.com>
2022-10-25 13:04:33 +0300

    some adds

--- src/main/java/org/example/SqlQueries.java
+++ src/main/java/org/example/SqlQueries.java
@@ -13,6 +13,7 @@ public final class SqlQueries {
             gender varchar(50),
             age int)
             """;
+
     public static final String INSERT_USERS = """
              INSERT INTO user (first_name, last_name, gender, age)
               VALUES ('Johnny', 'Depp', 'masculine', 59), ('Angelina', 'Jolie', 'feminine', 47),
@@ -21,19 +22,25 @@ public final class SqlQueries {
               ('Matt', 'LeBlanc', 'masculine', 55), ('Matthew', 'Perry', 'masculine', 53),
               ('Maria', 'Shevchenko', 'feminine', 12), ('Ivan', 'Sokyra', 'masculine', 15);
             """;
+
     public static String SELECT_UNDERAGE_USERS = """
             SELECT * FROM user WHERE age < 18
             """;
+
     public static String SELECT_NAME_ENDED_O = """
             SELECT * FROM user WHERE first_name LIKE '%o'
             """;
+
     public static String SELECT_BETWEEN_18_AND_60 = """
             SELECT * FROM user WHERE age BETWEEN 18 AND 60
             """;
+
     public static String SELECT_COUNT_NAME_HAVE_A = """
             SELECT COUNT(*) AS count FROM user WHERE first_name LIKE '%a%'
             """;
+
     public static String SELECT_ADULT_USERS = """
             SELECT COUNT(*) AS count FROM user WHERE age >=18
             """;
+
 }
--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -3,7 +3,6 @@ package org.example;
 import lombok.Data;
 
 @Data
-
 public class User {
     private Integer id;
     private String firstName;
